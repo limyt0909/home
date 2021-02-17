af6255a062d35a38f8dd53d6909d8232001331ac
LYT <limyt0909>
2021-02-17 17:10:39 +0900

    third

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express"); // express module을 import한다는 의미
 const ejs = require("ejs");
-const app = express(); // Express server의 시작
+const app = express(); // Creating the Express server
 const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
 
 
+// Server configuration
 app.set("view engine", "ejs"); // ejs 엔진을 사용한다고 선언하기
 // views들이 views 폴더에 저장됨을 설정
 app.set("views", path.join(__dirname, "views")); // app.set("views", __dirname + "/views"); 와 동일한 의미
 app.use(express.static(path.join(__dirname, "public"))); // css와 같은 static file들이 저장된 경로 설정
 
+
+// Starting the server
 app.listen(3000, () => {
   console.log("Server started (http://localhost:3000/) !");
 });
@@ -23,8 +27,7 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-
-
+// GET /data
 app.get("/data", (req, res) => {
   const test = {
     title: "Test",
@@ -34,8 +37,21 @@ app.get("/data", (req, res) => {
 
 });
 
-const sqlite3 = require("sqlite3").verbose();
 
+app.get("/books", (req, res) => {
+  const sql = "SELECT * FROM Books ORDER BY Title"
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("books", { model: rows });
+  });
+});
+
+
+
+
+//SQLite를 DB에 연결하는 코드
 const db_name = path.join(__dirname, "data", "apptest.db");
 const db = new sqlite3.Database(db_name, err => {
   if(err) {
@@ -44,3 +60,30 @@ const db = new sqlite3.Database(db_name, err => {
   console.log("Successful connection to the database 'apptest.db'");
 });
 
+
+const sql_create = `CREATE TABLE IF NOT EXISTS Books (
+  Book_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  Title VARCHAR(100) NOT NULL,
+  Author VARCHAR(100) NOT NULL,
+  Comments TEXT
+);`;
+
+db.run(sql_create, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful creation of the 'Books' table");
+});
+
+console.log("Successful creation of the 'Books' table");
+// Database seeding
+const sql_insert = `INSERT INTO Books (Book_ID, Title, Author, Comments) VALUES
+(1, 'Mrs. Bridge', 'Evan S. Connell', 'First in the serie'),
+(2, 'Mr. Bridge', 'Evan S. Connell', 'Second in the serie'),
+(3, 'L''ingénue libertine', 'Colette', 'Minne + Les égarements de Minne');`;
+db.run(sql_insert, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful creation of 3 books");
+});
